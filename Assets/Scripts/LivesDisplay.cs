b49ed982bd70692a6af214f10d9d16dc08144018
Stephen Fong <fongchinghinunsw@gmail.com>
2021-08-04 20:56:55 +0800

    Set music & difficulty options

--- Assets/Scripts/LivesDisplay.cs
+++ Assets/Scripts/LivesDisplay.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 
 public class LivesDisplay : MonoBehaviour
 {
-    [SerializeField] int lives = 5;
-    Text livesText;
+    [SerializeField] float baseLives = 3;
     [SerializeField] int damage = 1;
+    float lives;
+    Text livesText;
 
     // Start is called before the first frame update
     void Start()
     {
+        lives = baseLives - PlayerPrefsController.GetDifficulty();
         livesText = GetComponent<Text>();
         UpdateDisplay();
+        Debug.Log("difficulty setting currently is " + PlayerPrefsController.GetDifficulty());   
     }
 
     private void UpdateDisplay()
